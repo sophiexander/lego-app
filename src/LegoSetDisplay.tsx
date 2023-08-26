364a294b3b851e988b32c59536728833b409dce2
sophiexander <sophie3606@gmail.com>
2023-08-26 20:57:40 +1200

    add next button

--- src/LegoSetDisplay.tsx
+++ src/LegoSetDisplay.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { MouseEventHandler, useState } from "react";
 import axios from "axios";
 import { LegoSet, LegoSetResponseType } from "./models/LegoSet";
 import LegoSetCard from "./LegoSetCard";
@@ -9,25 +9,28 @@ const legoSetNumbers = ["10312-1"];
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 export default function LegoSetDisplay({}: Props) {
+  const [LegoURL, setLegoURL] = useState(
+    `https://rebrickable.com/api/v3/lego/sets/?key=${API_KEY}&min_year=2000&ordering=theme_id`
+  );
   const [legoSetArray, setLegoSetArray] = useState<LegoSet[] | undefined>();
+
+  let next: string | null = "";
 
   React.useEffect(() => {
     getLegoSet();
-  }, []);
+  }, [LegoURL]);
 
   async function getLegoSet() {
     try {
-      const { data, status } = await axios.get<LegoSetResponseType>(
-        `https://rebrickable.com/api/v3/lego/sets/?key=${API_KEY}`,
-        {
-          headers: {
-            Accept: "application/json",
-          },
-        }
-      );
+      const { data, status } = await axios.get<LegoSetResponseType>(LegoURL, {
+        headers: {
+          Accept: "application/json",
+        },
+      });
       console.log(JSON.stringify(data, null, 4));
       console.log("response status is: ", status);
       setLegoSetArray(data.results);
+      next = data.next;
     } catch (error) {
       if (axios.isAxiosError(error)) {
         console.log("error message: ", error.message);
@@ -39,8 +42,16 @@ export default function LegoSetDisplay({}: Props) {
     }
   }
 
+  function handleClick(event: any) {
+    console.log("click next page");
+    if (next) {
+      setLegoURL(next);
+    }
+  }
+
   return (
     <div className="uk-flex uk-flex-wrap">
+      <button onClick={handleClick}>Next Page</button>
       {legoSetArray?.map((item, i) => (
         <LegoSetCard key={i} set={item} />
       ))}
